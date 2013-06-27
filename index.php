e0ce70191de6168e1e1ca022e5581df474f854cb
Alex <alex19891013@gmail.com>
2013-06-27 15:28:58 +0200

    simu

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	$_server = isset($_GET['server'])?$_GET['server']:"";
 	$_interval = isset($_GET['interval'])?$_GET['interval']:"60";
 	
-	$device_array = array("ios", "android", "wp");
+	$device_array = array("ios", "android", "wp", "server");
 	$server_array = array("A", "B", "C", "D");
 	
 	if(!in_array($_device, $device_array) && $_device != ""){
