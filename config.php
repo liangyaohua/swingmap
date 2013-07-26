4497064ba02070b8b369238a9f7c4a47a5f3f59b
Alex <alex19891013@gmail.com>
2013-07-26 10:47:53 +0200

    connect and adapt to the real server

--- config.php
+++ config.php
@@ -1,4 +1,11 @@
 <?php
+	error_reporting(E_ERROR);
+	define('HOST','http://'.$_SERVER['HTTP_HOST'].'/swingmap');
+	define('DEBUG',true); // set true for test, false for live
+	date_default_timezone_set('Europe/Paris');
+	
+	$simulator = false;
+	
 	$device_array = array("ios", "android", "wp", "server");
 	$server_array = array("A", "B", "C", "D");
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
-	error_reporting(E_ERROR);
 	define('ROOT_PATH', preg_replace("/\\\/",'/', dirname(__FILE__)).'/');
-	define('HOST','http://'.$_SERVER['HTTP_HOST'].'/swingmap');
-	define('DEBUG',true); // set true for test, false for live
-	date_default_timezone_set('Europe/Paris');
-	
+
 	include_once(ROOT_PATH.'config.php');
 
 	include_once(ROOT_PATH.'view/swingmap.htm');
